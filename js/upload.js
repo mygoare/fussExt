39b9d3d0e4f93f3c28eba8bb6a2636c05dfca424
mygoare <mygoare@gmail.com>
2012-12-01 12:43:41 +0800

    opt select your picture your upload begin

--- js/upload.js
+++ js/upload.js
@@ -27,19 +27,21 @@ $(function(){
           //document.execCommand('copy');
           $("#eleme_pic_info").html('');
           $('textarea:last').val($('textarea:last').val()+img_url);
+          // animote
+          $('#eleme_pic_upload').animate({'left':'-150px'});
         }else {
           $("#eleme_pic_info").html('<span style="color:red;">error!</span>');
         }
       }
     };
     $("#myform").ajaxForm(options);
+    
+    $("#eleme_pic_upload_file").change(function(){
+      $("#myform").submit();
+    });
 
     // upload block movement
     $('#eleme_pic_upload').hover(function(){
       $(this).animate({'left':'10px'});
-    },function(){
-      $('#eleme_pic_upload_button').click(function(){
-        $('#eleme_pic_upload').animate({'left':'-150px'});
-      });
     });
 });
